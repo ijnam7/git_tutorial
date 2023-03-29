cfc5df654d3c9279061a8b53a6e93c6124250fae
ijnam7 <118372604+ijnam7@users.noreply.github.com>
2023-03-29 10:10:45 +0900

    GitHub for git pull by ij nam

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 // For command line git tutorial
 // Local repository modification
 // GitHub modification
+// Third : git commit --amend
 
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
