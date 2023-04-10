5622d046974992991357d6e5c26dc769a5d5c24e
ijnam7 <ijnam7@gmail.com>
2023-04-10 10:48:00 +0900

    Revert "GitHub for git pull by ij nam"
    This reverts commit cfc5df654d3c9279061a8b53a6e93c6124250fae.

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
 // For command line git tutorial
 // Local repository modification
 // GitHub modification
-// Third : git commit --amend
 
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
-print("Tell my world")
